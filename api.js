5e159868a998cb89224104b929f9818218d0130e
s-ota421 <160009631+s-ota421@users.noreply.github.com>
2026-05-15 07:22:26 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,9 +1,20 @@
 const GAS_URL = "https://script.google.com/macros/s/AKfycbxqqvWP2MzB-Yn-BruS08BrGh_TsI2YBy7yRcVptcWtSKlJapD1eGDv99DPruUVcEh--g/exec";
+//利用者取得
 async function getUserName(id) {
 
   const res =
     await fetch(
       `${GAS_URL}?type=user&id=${id}`
+    );
+
+  return await res.json();
+}
+??体調データ取得
+async function getConditionData(id) {
+
+  const res =
+    await fetch(
+      `${GAS_URL}?type=condition&id=${id}`
     );
 
   return await res.json();
