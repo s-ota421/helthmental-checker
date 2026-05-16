ac6e4b274e2e398b2a74f845dc7a07c00d4ad8a8
s-ota421 <160009631+s-ota421@users.noreply.github.com>
2026-05-16 14:56:39 +0900

    Update input.js

--- input.js
+++ input.js
@@ -1,5 +1,5 @@
 const res = await fetch(
-  "https://script.google.com/macros/s/AKfycbyDC4-v8DXFlOcdi2MBMJL5Zm2fGc4INRHTj-CEyQ0H-q-V4nBb9Xf2qNplu5ww3on6qw/exec",
+  "https://script.google.com/macros/s/AKfycbxqqvWP2MzB-Yn-BruS08BrGh_TsI2YBy7yRcVptcWtSKlJapD1eGDv99DPruUVcEh--g/exec",
 
 let currentUser = null;
 const params = new URLSearchParams(location.search);
