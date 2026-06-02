46c93ec202b9685499b2bf65c32db168ab7ca2bd
s-ota421 <160009631+s-ota421@users.noreply.github.com>
2026-06-02 11:29:01 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzmvCnlGFEDM6u4lLuWAJjxGebucapTzd1WY2SpJxC49rh5EhLRmAhIxtLxCQC-gghVNg/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyDC4-v8DXFlOcdi2MBMJL5Zm2fGc4INRHTj-CEyQ0H-q-V4nBb9Xf2qNplu5ww3on6qw/exec";
 
 async function getUserName(id) {
   const res = await fetch(GAS_URL + "?type=user&id=" + encodeURIComponent(id));
